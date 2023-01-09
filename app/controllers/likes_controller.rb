0653c9c495514ae3eacb0485eded7f2720a79c60
kangzy-fix <kangzy.fix@student.moringaschool.com>
2023-01-09 12:31:36 +0300

    updated like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,7 @@
 class LikesController < ApplicationController
+    def create
+        @post = Post.find(params[:post_id])
+        @like = current_user.likes.create(post: @post)
+        redirect_to @post
+      end
 end
