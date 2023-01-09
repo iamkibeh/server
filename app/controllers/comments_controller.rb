0c0329c3c3f375163c1d4291a0811c0989355f7e
jeffoty <odhiambo47@gmail.com>
2023-01-09 12:25:05 +0300

    Added more method to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,19 @@ class CommentsController < ApplicationController
     end
 
     def create
-        
+        comments = Comments.create(comments_params)
+        render json: comments, status: :created
+    end
+
+    def destroy
+        comments = Comments.find(params[:id])
+        comments.destroy
+        head :no_content
+    end
+
+    private
+
+    def comments_params
+        params.permit(:comment)
     end
 end
