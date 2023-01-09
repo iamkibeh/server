1dafa8f5226b9f9a79408568bab9598b6acf26a6
jeffoty <odhiambo47@gmail.com>
2023-01-09 12:25:33 +0300

    Added more routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :posts
-  resources :comments
-  resources :users
+  resources :likes, only: [:index, :create]
+  resources :posts, only: [:index, :create, :show, :destroy]
+  resources :comments, only: [:index, :create, :destroy]
+  resources :users, only: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
