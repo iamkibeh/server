989e14b02a9c3f98b14f66d4b47f021c9ba08858
jeffoty <odhiambo47@gmail.com>
2023-01-10 14:02:32 +0300

    minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :comments, dependent: :destroy
     has_many :post, through: :comments
-    has_one_attached :image
+    has_one_attached :avatar
 end
     
