24d6be75d389a8fc094189a7a9ca541af70f81c6
kangzy-fix <kangzy.fix@student.moringaschool.com>
2023-01-10 11:45:31 +0300

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :post_types
   resources :likes, only: [:index, :create]
   resources :posts, only: [:index, :create, :show, :destroy]
-  resources :comments, only: [:index, :create, :destroy]
-  resources :users, only: [:index, :create]
+  resources :comments, only: [:index, :create, :show, :destroy]
+  resources :users, only: [:index, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
