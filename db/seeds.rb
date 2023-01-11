4817aec61c4578d87d89c15259c44c65527e0e82
kangzy-fix <kangzy.fix@student.moringaschool.com>
2023-01-11 10:31:29 +0300

    Added users data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,151 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Hold on a moment... 😎️"
 
 
+User.create(
+    first_name: "Ian",
+    last_name: "Kangacha",
+    email: "kangzyfix@gmail.com",
+    phone_number: "+254743828153",
+    password_digest: "password",
+    agreement: "agreement",
+    github: "https://github.com/kangzy-fix",
+    linkedin: "https://www.linkedin.com/in/ian-kangacha-56101b245/",
+    cv_link: "https://mycv.com/",
+    cover_img: "mycover.jpg",
+    country: "Kenya",
+    bio: "This is my bio",
+    skills: "Javascript, Ruby on rails, react",
+    profession: "Programming",
+    company_id: "123456789",
+    company_website: "http://moringaalumini.com",
+    created_at: "20231201659",
+    updated_at: "20231201659"
+  )
+  
+  User.create(
+    first_name: "Denis",
+    last_name: "Ouma",
+    email: "denisouma@gmail.com",
+    phone_number: "+254777730535",
+    password_digest: "password",
+    agreement: "agreement",
+    github: "https://github.com/DenisOuma/Denisouma",
+    linkedin: "https://www.linkedin.com/in/denisouma/",
+    cv_link: "https://mycv.com/",
+    cover_img: "mycover.jpg",
+    country: "Kenya",
+    bio: "This is my bio",
+    skills: "Javascript, Ruby on rails, react",
+    profession: "Programming",
+    company_id: "123456789",
+    company_website: "http://moringaalumini.com",
+    created_at: "20231201659",
+    updated_at: "20231201659"
+  )
+  User.create(
+    first_name: "Jeff",
+    last_name: "Odhiambo",
+    email: "odhiambo47@gmail.com",
+    phone_number: "+254722418808",
+    password_digest: "password",
+    agreement: "agreement",
+    github: "https://github.com/jeffoty",
+    linkedin: "https://www.linkedin.com/in/jeff-odhiambo-07822b242/",
+    cv_link: "https://mycv.com/",
+    cover_img: "mycover.jpg",
+    country: "Kenya",
+    bio: "This is my bio",
+    skills: "Javascript, Ruby on rails, react",
+    profession: "Programming",
+    company_id: "123456789",
+    company_website: "http://moringaalumini.com",
+    created_at: "20231201659",
+    updated_at: "20231201659"
+  )
+  User.create(
+    first_name: "Immanuel",
+    last_name: "Kibet",
+    email: "immanuelkibet@gmail.com",
+    phone_number: "+254743828153",
+    password_digest: "password",
+    agreement: "agreement",
+    github: "https://github.com/iamkibeh",
+    linkedin: "https://www.linkedin.com/in/kibet-immanuel/",
+    cv_link: "https://mycv.com/",
+    cover_img: "mycover.jpg",
+    country: "Kenya",
+    bio: "This is my bio",
+    skills: "Javascript, Ruby on rails, react",
+    profession: "Programming",
+    company_id: "123456789",
+    company_website: "http://moringaalumini.com",
+    created_at: "20231201659",
+    updated_at: "20231201659"
+  )
+  User.create(
+    first_name: "Lucy",
+    last_name: "Okoth",
+    email: "lucyokoth@gmail.com",
+    phone_number: "+254795866729",
+    password_digest: "password",
+    agreement: "agreement",
+    github: "https://github.com/Lucy-okoth",
+    linkedin: "https://www.linkedin.com/in/lucy-okoth-33564a238/",
+    cv_link: "https://mycv.com/",
+    cover_img: "mycover.jpg",
+    country: "Kenya",
+    bio: "This is my bio",
+    skills: "Javascript, Ruby on rails, react",
+    profession: "Programming",
+    company_id: "123456789",
+    company_website: "http://moringaalumini.com",
+    created_at: "20231201659",
+    updated_at: "20231201659"
+  )
 
+  User.create(
+    first_name: "Victor",
+    last_name: "Muhatia",
+    email: "victormuhatia@gmail.com",
+    phone_number: "+254741689150",
+    password_digest: "password",
+    agreement: "agreement",
+    github: "",
+    linkedin: "",
+    cv_link: "https://mycv.com/",
+    cover_img: "mycover.jpg",
+    country: "Kenya",
+    bio: "This is my bio",
+    skills: "Javascript, Ruby on rails, react",
+    profession: "Programming",
+    company_id: "123456789",
+    company_website: "http://moringaalumini.com",
+    created_at: "20231201659",
+    updated_at: "20231201659"
+  )
 
+  User.create(
+    first_name: "Andrew",
+    last_name: "Kigen",
+    email: "kigenandrew@gmail.com",
+    phone_number: "+254715614452",
+    password_digest: "password",
+    agreement: "agreement",
+    github: "https://github.com/kigen27",
+    linkedin: "https://www.linkedin.com/in/andrew-kigen-7bb38024b/",
+    cv_link: "https://mycv.com/",
+    cover_img: "mycover.jpg",
+    country: "Kenya",
+    bio: "This is my bio",
+    skills: "Javascript, Ruby on rails, react",
+    profession: "Programming",
+    company_id: "123456789",
+    company_website: "http://moringaalumini.com",
+    created_at: "20231201659",
+    updated_at: "20231201659"
+  )
 
-
-# db/seeds.rb
-
-
-
-
-
+puts "Done creating users data....😍️"
