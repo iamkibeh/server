5ec4c10057b8496e6141a125f50438b529483603
jeffoty <odhiambo47@gmail.com>
2023-01-15 01:22:51 +0300

    added actions before CRUD operations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_method
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_method
+    skip_before_action :authenticate_user, only: [:create]
+    before_action :find_user, only: [:show, :update, :destroy]
+
 
     def index
         users = User.all
@@ -25,6 +28,11 @@ rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_method
         end
     end
 
+    #def update
+     #   unless User.update(user_params)
+      #      render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+    #end
+
     private
 
     def find_user
@@ -38,6 +46,5 @@ rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_method
     def record_not_found_method
         render json: { error: "Destination not found" }, status: :not_found
     end
-
 
 end
