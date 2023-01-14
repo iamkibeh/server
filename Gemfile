08c5f764d5b3626c32b311befbcc53b70bfb318b
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-01-15 00:16:24 +0300

    install deps

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.7.2'
+ruby '2.7.4'
 
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
-gem 'rails', '~> 6.1.7'
+gem 'rails', '~> 7.0.4'
 
 gem 'cloudinary'
 
--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 2023_01_10_053509) do
   end
 
   create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_types", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
