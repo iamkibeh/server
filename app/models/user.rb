4f1eef9f5641673c7586126352cc4337229b6575
jeffoty <odhiambo47@gmail.com>
2023-01-16 09:50:09 +0300

    Added validations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,21 @@ class User < ApplicationRecord
     has_one_attached :avatar
 
    
-    validates :password, presence: true
+    validates :password, format: { with: /\A[\w-]+\z/, message: "your format requirements" }
     validates :email, presence: true, uniqueness: true
+    validate :password_requirements_are_met
+
+    def password_requirements_are_met
+        rules = {
+          " must contain at least one lowercase letter"  => /[a-z]+/,
+          " must contain at least one uppercase letter"  => /[A-Z]+/,
+          " must contain at least one digit"             => /\d+/,
+          " must contain at least one special character" => /[^A-Za-z0-9]+/
+        }
+      
+        rules.each do |message, regex|
+          errors.add( :password, message ) unless password.match( regex )
+        end
+      end
 end
     
